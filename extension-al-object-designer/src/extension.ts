70fe5d4730deaac2adba1cb0700d2c4add66d19a
Márton Sági <marton@dynasist.com>
2019-01-04 09:09:27 +0100

    #11 logging debug info for investigation

--- extension-al-object-designer/src/extension.ts
+++ extension-al-object-designer/src/extension.ts
@@ -7,11 +7,21 @@ import { ALObjectDesigner } from './ALModules';
 // this method is called when your extension is activated
 export function activate(context: vscode.ExtensionContext) {
     context.subscriptions.push(vscode.commands.registerCommand('extension.openALWindow', async () => {
-        await ALPanel.open(context.extensionPath, ALObjectDesigner.PanelMode.List);
+        try {
+            await ALPanel.open(context.extensionPath, ALObjectDesigner.PanelMode.List);
+        } catch (e) {
+            console.error(e);
+            vscode.window.showErrorMessage(`AL Object Designer could not be opened. Error: '${e.message}'`);
+        }
     }));
 
     context.subscriptions.push(vscode.commands.registerCommand('extension.openALDesignWindow', async () => {
-        await ALPanel.openDesigner(context.extensionPath);
+        try {
+            await ALPanel.openDesigner(context.extensionPath);
+        } catch (e) {
+            console.error(e);
+            vscode.window.showErrorMessage(`AL Page Designer could not be opened. Error: '${e.message}'`);
+        }
     }));
 }
 
