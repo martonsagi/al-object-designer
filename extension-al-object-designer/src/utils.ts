532a82bc343039709e91dbebf2fa4d52a3494efd
Márton Sági <marton@dynasist.com>
2019-11-14 09:30:47 +0100

    Second fix attempt #37, new alpha build

--- extension-al-object-designer/src/utils.ts
+++ extension-al-object-designer/src/utils.ts
@@ -145,7 +145,8 @@ export async function getFirstCodeLine(file: string) {
 
 export async function getObjectHeaders(filePath: string) {
     let fileContent: string = await read(filePath) as string;
-    let pattern = /([a-z]+)\s([0-9]+|.*?)\s?(.*)/gm;
+    //let pattern = /([a-z]+)\s([0-9]+|.*?)\s?(.*)/gm;
+    let pattern = /\b^(codeunit|page|pageextension|pagecustomization|dotnet|enum|enumextension|query|report|table|tableextension|xmlport|profile|controladdin)\s([0-9]+|.*?)\s?(.*)/gm;
     let matches = getAllMatches(pattern, fileContent);
 
     let result = matches.map(m => m[0]);
